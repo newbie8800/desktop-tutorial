57a1e2e2d955aa07b269fe0843f3a153014da97c
newbie8800 <83655036+newbie8800@users.noreply.github.com>
2021-06-11 21:38:06 -0700

    Add files via upload

---
+++ trade.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+
+    var params1 = (new URL(document.location)).searchParams;
+  
+    var thetext1 = params1.get('textarea');
+
+  
+   
+    
+
+    document.getElementById('result-textarea').innerHTML = thetext1;
+ 
+//////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////
+
+    
+    })
